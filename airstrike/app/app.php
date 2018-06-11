7626e4f0919fb7e5b98e95931444ccd166b6d3e4
xTxMiguelxTx <39289775+xTxMiguelxTx@users.noreply.github.com>
2018-06-10 22:36:57 -0600

    .

--- airstrike/app/app.php
+++ airstrike/app/app.php
@@ -30,9 +30,6 @@ include 'apps/PaisApp.php';
 include 'apps/PermisoApp.php';
 include 'apps/RolApp.php';
 include 'apps/TipoPermisoApp.php';
-<<<<<<< HEAD
 include 'apps/VueloApp.php';
 include 'apps/ProgramacionVueloApp.php';
-=======
 include 'apps/UsuarioApp.php';
->>>>>>> cd50926fab22425e1e8c823226b41328904e3b38
